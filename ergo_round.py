47c00b2e2c1b827b6a7b4c3e55032378c096dc0d
Jeroen <jpvermeulen@hotmail.com>
2025-04-13 13:46:21 +0200

    extended examples with edge cases from full dataset

--- ergo_round.py
+++ ergo_round.py
@@ -17,8 +17,8 @@ date_ranges = {('1989-07-01', '1993-09-01', 'Rotterdam Study 1'): ('e1', 1),  # 
                ('2015-04-01', '2016-05-31', 'Rotterdam Study 2'): ('e6', 4),
                ('2016-06-01', '2020-12-31', 'Rotterdam Study 4'): ('ex', 1),
                ('2018-04-01', '2019-12-31', 'Rotterdam Study 1'): ('e7', 7),
-               ('2021-03-01', '2028-01-01', 'Rotterdam Study 2'): ('e8', 1),
-               ('2021-03-01', '2028-01-01', 'Rotterdam Study 3'): ('e8', 2)}
+               ('2021-03-01', '2024-09-30', 'Rotterdam Study 2'): ('e8', 5),
+               ('2021-03-01', '2024-09-30', 'Rotterdam Study 3'): ('e8', 2)}
 
 # Creates a lookup per project_name, visit_nr
 visit_order_lookup = {
@@ -36,15 +36,20 @@ def choose_prefix(row,
                   visit_number_column='visit_nr'):
     
     '''
-    Choose the correct round prefix from the date_ranges (dct) -ergo wiki-, based on the visit number and the cohort name.
-    If no match is found for the visit, then determine the minimum distance to the mid point for other visits.
+    Choose the correct round prefix from the date_ranges (dct) from -ergo wiki-, based on the visit number and the cohort name.
+    If no match is found for the current visit, then proceed with other checks:
+    2) Check the distance to the midpoint of the current visit 
+    3) Check the distance to the midpoint of the next visit 
+    4) If 2) and 3) are not within the threshold of 3 years, check all the dates. Multiple visits are missing
     '''
     
     closest_distance = float('inf')
+    closest_distance_threshold = 1096 # 3 years 
     closest_prefix = None
 
+    # 1. Direct match. If no visits are missing, this will yield a match
     for (start_date, end_date, project_name), (prefix, visit_number) in date_ranges.items():
-        # Direct match
+        
         if (start_date <= row[date_column] <= end_date) and \
            (row[study_column] == project_name) and \
            (row[visit_number_column] == visit_number):
@@ -55,7 +60,7 @@ def choose_prefix(row,
     study_id = row[study_column]
     visit_nr = row[visit_number_column]
 
-    # Check distances to midpoint of current visit_nr
+    # 2. Check distances to midpoint of current visit
     for (start_date, end_date, project_name), (prefix, visit_number) in date_ranges.items():
         if project_name == study_id and visit_number == visit_nr:
             midpoint = start_date + (end_date - start_date) / 2
@@ -64,7 +69,7 @@ def choose_prefix(row,
                 closest_distance = distance
                 closest_prefix = prefix
 
-    # Check distances to midpoint of next visit_nr ranges
+    # 3. Check distances to midpoint of next visit
     for (start_date, end_date, project_name), (prefix, visit_number) in date_ranges.items():
         if project_name == study_id and visit_number == visit_nr + 1:
             midpoint = start_date + (end_date - start_date) / 2
@@ -73,18 +78,28 @@ def choose_prefix(row,
                 closest_distance = distance
                 closest_prefix = prefix 
 
+    # 4. Check distances to midpoint for all visits
+    if closest_distance > closest_distance_threshold:  # 5 years in days
+        for (start_date, end_date, project_name), (prefix, visit_number) in date_ranges.items():
+            if project_name == study_id:
+                midpoint = start_date + (end_date - start_date) / 2
+                distance = abs((visit_date - midpoint).days)
+                if distance < closest_distance:
+                    closest_distance = distance
+                    closest_prefix = prefix
     return closest_prefix
 
 
-def correct_rounds_from_lookup(df, round_column='round'):
+def correct_rounds_from_lookup(df, id_column='ergo_id', round_column='round'):
     '''
     If no exact match is found in choose_prefix() and the midpoints are used to determine the closest visit,
-    it is recommended to check the visit order and correct possible mistakes. This could be the case, especially for: RS-I-1, RS-II-2, RS-II-3. 
+    it is recommended to check the visit order. This could be the case, especially for: RS-I-1, RS-II-2, RS-II-3. 
+    This function checks for duplicated rounds and corrects them using the visit_order_lookup
     '''
     df_ = df.copy()
-    df_ = df_.sort_values(['ergo_id', 'study_id', 'visit_date'])
+    df_ = df_.sort_values([id_column, 'study_id', 'visit_date'])
 
-    for (ergo_id, study_id), group in df_.groupby(['ergo_id', 'study_id']):
+    for (ergo_id, study_id), group in df_.groupby([id_column, 'study_id']):
         round_counts = group[round_column].value_counts()
         duplicated_rounds = round_counts[round_counts > 1].index.tolist()
 
